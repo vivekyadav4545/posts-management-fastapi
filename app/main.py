b350c5bb3dad5be9e785ef831c9f4380d3cc69a8
vivekyadav4545 <vy959818@gmail.com>
2026-07-22 14:41:03 +0530

    Redirect root to swagger docs

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from .import model
 from .database import engine
 from .routers import post,user,auth,vote
 from .config import settings
+from fastapi.responses import RedirectResponse
 
 
 
@@ -28,6 +29,13 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+
+@app.get("/")
+def root():
+    return RedirectResponse(url="/docs")
+
+
 
 @app.get("/")
 def root():
